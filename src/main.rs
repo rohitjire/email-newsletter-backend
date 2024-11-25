90cd53d41c58efa63030f6c3e6100784c73f3f29
Rohit Jire <rohitjire55@gmail.com>
2024-11-25 22:45:35 +0100

    added api logger and submodules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware::Logger, web, App, HttpServer, Responder};
+
+mod utils;
+mod routes;
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+
+    
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+        .wrap(Logger::default())
+        .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
