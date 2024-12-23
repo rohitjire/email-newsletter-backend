5846b84c8d718ed35c29440cf3642b0c70a31165
Ankita Tiwari <ankitajtiwari18@gmail.com>
2024-12-23 20:12:37 +0100

    WIP login user API

--- src/routes/handlers/auth_handlers.rs
+++ src/routes/handlers/auth_handlers.rs
@@ -1,16 +1,26 @@
 use actix_web::{post, web, Responder};
-
+use sea_orm::Condition;
+use sea_orm::EntityTrait;
+use sea_orm::ColumnTrait;
+use sea_orm::QueryFilter;
 use sea_orm::Set;
 use sea_orm::ActiveModelTrait;
 use serde::Deserialize;
 use serde::Serialize;
 use sha256::digest;
 
+use crate::utils::jwt::encode_jwt;
 use crate::utils::{api_response, app_state};
 
 #[derive(Serialize, Deserialize)]
 struct RegisterModel {
     name: String,
+    email: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct LoginModel {
     email: String,
     password: String
 }
@@ -29,7 +39,29 @@ pub async fn register(
         }.insert(&app_state.db).await.unwrap();
 
         api_response::ApiResponse::new(200, format!("{}", user_model.id))
-}       
+}
 
+#[post("/login")]
+pub async fn login(
+    app_state: web::Data<app_state::AppState>,
+    login_json:web::Json<LoginModel> ) -> impl Responder {
+        
+        let user= entity::user::Entity::find()
+        .filter(
+            Condition::all()
+            .add(entity::user::Column::Email.eq(&login_json.email))
+            .add(entity::user::Column::Password.eq(digest(&login_json.password)))
+        ).one(&app_state.db).await.unwrap();
+
+        if user.is_none() {
+            return api_response::ApiResponse::new(401, "User not found".to_string());
+        }
+
+        let user_data = user.unwrap();
+
+        let token = encode_jwt(user_data.email, user_data.id ).unwrap();
+
+        api_response::ApiResponse::new(200, format!("{{'token': '{}'}}", token))
+    }
 
 
--- src/utils/contants.rs
+++ src/utils/contants.rs
@@ -6,6 +6,7 @@ lazy_static! {
     pub static ref HOST_ADDRESS: String = set_host_address();
     pub static ref PORT: u16 = set_port();
     pub static ref DATABASE_URL: String = set_database_url();
+    pub static ref SECRET: String = set_secret();
 }
 
 fn set_host_address() -> String {
@@ -24,4 +25,9 @@ fn set_port() -> u16 {
 fn set_database_url() -> String {
     dotenv::dotenv().ok();
     env::var("DATABASE_URL").unwrap()
+}
+
+fn set_secret() -> String {
+    dotenv::dotenv().ok();
+    env::var("SECRET").unwrap()
 }
