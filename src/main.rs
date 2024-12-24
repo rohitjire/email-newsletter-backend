810f3a3ad9b7d658260eb614b6b85fd6c7474943
Rohit Jire <rohitjire55@gmail.com>
2024-12-24 17:58:58 +0100

    fixed minor issue

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .configure(routes::home_routes::config)
             .configure(routes::auth_routes::config)
+            .configure(routes::user_routes::config)
            })
     .bind((host_address, port))?
     .run()
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,3 +1,5 @@
 pub mod home_routes;
 pub mod auth_routes;
+pub mod user_routes;
+pub mod middlewares;
 pub mod handlers;
