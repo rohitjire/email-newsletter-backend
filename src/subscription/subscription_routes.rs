5da60880ecd1d8059a6fc1047b6f76b0be6e19f9
Chirag <chiragnabhoya2506@gmail.com>
2025-02-14 17:04:49 +0100

    add subscription_routes.rs Documentation

--- src/subscription/subscription_routes.rs
+++ src/subscription/subscription_routes.rs
@@ -1,15 +1,25 @@
+/// Routes configuration for subscription endpoints.
+/// This module registers routes for subscribing and unsubscribing users with authentication.
 use actix_web::{middleware::from_fn, web};
-
 use crate::middlewares;
-
 use super::subscription_handlers;
 
-
+/// Configures routes for subscription-related operations.
+/// 
+/// # Routes:
+/// - `/subscription/subscribe-user`: Subscribe to a user (requires authentication).
+/// - `/subscription/unsubscribe-user`: Unsubscribe from a user (requires authentication).
+/// 
+/// # Middleware:
+/// - `auth_middlewares::check_auth_middleware`: Ensures user authentication.
+/// 
+/// # Arguments:
+/// * `config` - Actix Web `ServiceConfig` to which routes are added.
 pub fn config(config: &mut web::ServiceConfig) {
     config.service(
         web::scope("/subscription")
-            .wrap(from_fn(middlewares::auth_middlewares::check_auth_middleware))
-            .service(subscription_handlers::subscribe_user)
-            .service(subscription_handlers::unsubscribe_user)
+            .wrap(from_fn(middlewares::auth_middlewares::check_auth_middleware))// Auth Middleware
+            .service(subscription_handlers::subscribe_user)// Endpoint to subscribe a user
+            .service(subscription_handlers::unsubscribe_user)// Endpoint to unsubscribe a user
     );
 }
