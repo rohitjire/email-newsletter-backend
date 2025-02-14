9480e3952994c169896fe0c4f8da694ed644206f
Chirag <chiragnabhoya2506@gmail.com>
2025-02-14 12:53:22 +0100

    Add mod.rs Documentation

--- src/article/mod.rs
+++ src/article/mod.rs
@@ -1,2 +1,8 @@
+/// Module declarations for `article` components.
+/// This module exposes handlers and route configurations.
+
+/// Module for article-related request handlers.
 pub mod article_handlers;
+
+/// Module for article-related route configurations.
 pub mod article_routes;
