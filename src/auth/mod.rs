d008f2ce3bc4b9eb975fdc283eb3b5e55bff5161
Chirag <chiragnabhoya2506@gmail.com>
2025-02-14 14:01:28 +0100

    Add mod.rs Documentation

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,2 +1,8 @@
+/// Module declarations for `auth` components.
+/// This module exposes handlers and route configurations.
+
+/// Module for authentication request handlers.
 pub mod auth_handlers;
+
+/// Module for authentication route configurations.
 pub mod auth_routes;
