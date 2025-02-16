bf49fa5e4dea3948234824268d43a5abd632bcb8
Rohit Jire <rohitjire55@gmail.com>
2025-02-16 23:18:41 +0100

    updated app_state in auth_handler.rs

--- src/auth/auth_handlers.rs
+++ src/auth/auth_handlers.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use actix_web::{post, web};
 use sea_orm::ActiveModelTrait;
 use sea_orm::ColumnTrait;
@@ -14,16 +16,16 @@ use crate::utils::jwt::encode_jwt;
 use crate::utils::{api_response, app_state};
 
 #[derive(Serialize, Deserialize)]
-struct RegisterModel {
-    name: String,
-    email: String,
-    password: String,
+pub struct RegisterModel {
+    pub name: String,
+    pub email: String,
+    pub password: String,
 }
 
 #[derive(Serialize, Deserialize)]
-struct LoginModel {
-    email: String,
-    password: String,
+pub struct LoginModel {
+    pub email: String,
+    pub password: String,
 }
 
 #[post("/register")]
@@ -31,13 +33,16 @@ pub async fn register(
     app_state: web::Data<app_state::AppState>,
     register_json: web::Json<RegisterModel>,
 ) -> Result<ApiResponse, ApiResponse> {
+
+    let db = Arc::clone(&app_state.db);
+
     let user_model = entity::user::ActiveModel {
         name: Set(register_json.name.clone()),
         email: Set(register_json.email.clone()),
         password: Set(digest(&register_json.password)),
         ..Default::default()
     }
-    .insert(&app_state.db)
+    .insert(&*db)
     .await
     .map_err(|err| ApiResponse::new(500, err.to_string()))?;
 
@@ -52,13 +57,16 @@ pub async fn login(
     app_state: web::Data<app_state::AppState>,
     login_json: web::Json<LoginModel>,
 ) -> Result<ApiResponse, ApiResponse> {
+
+    let db = Arc::clone(&app_state.db);
+
     let user_data = entity::user::Entity::find()
         .filter(
             Condition::all()
                 .add(entity::user::Column::Email.eq(&login_json.email))
                 .add(entity::user::Column::Password.eq(digest(&login_json.password))),
         )
-        .one(&app_state.db)
+        .one(&*db)
         .await
         .map_err(|err| ApiResponse::new(500, err.to_string()))?
         .ok_or(ApiResponse::new(404, "User not Found".to_owned()))?;
