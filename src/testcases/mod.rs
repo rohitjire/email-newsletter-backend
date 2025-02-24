ca65eaf99666ede1366eba0a564526ef5c1ff947
Ankita Tiwari <ankitajtiwari18@gmail.com>
2025-02-24 06:57:21 +0100

    created test_get_user

--- src/testcases/mod.rs
+++ src/testcases/mod.rs
@@ -1,4 +1,5 @@
 pub mod auth_handler_test;
 pub mod article_handlers_test;
 pub mod subscription_handlers_test;
+pub mod user_handlers_test;
 
---
+++ src/testcases/user_handlers_test.rs
@@ -0,0 +1,45 @@
+#[cfg(test)]
+pub mod tests {
+    use std::sync::Arc;
+
+    use crate::{
+        user::user_routes::config,
+        utils::{app_state::AppState, jwt::encode_jwt},
+    };
+    use actix_web::{http::StatusCode, test, web, App};
+    use sea_orm::{DatabaseBackend, MockDatabase};
+    use serial_test::serial;
+
+    #[actix_web::test]
+    #[serial]
+    pub async fn test_get_user() {
+        let token = encode_jwt("author@test.com".to_string(), 1).unwrap();
+        let mock_db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results(vec![vec![entity::user::Model {
+                id: 1,
+                name: "Author".to_string(),
+                email: "author@test.com".to_string(),
+                password: "12345".to_string(),
+            }]])
+            .into_connection();
+
+        let mock_db = Arc::new(mock_db);
+
+        let app_state = web::Data::new(AppState {
+            db: Arc::clone(&mock_db),
+        });
+
+        let app =
+            test::init_service(App::new().app_data(app_state.clone()).configure(config)).await;
+
+        let req = test::TestRequest::get()
+            .uri("/user/get-user")
+            .insert_header(("Content-Type", "application/json"))
+            .insert_header(("Authorization", format!("Bearer {}", token)))
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}
